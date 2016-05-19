3b168e4c6d17aba92a83ee56b7855708461df8e0
kithaca <kithaca@gmail.com>
2016-05-19 14:51:38 -0700

    hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,15 +4,32 @@ end
 
 class Array
   def hash
+  	hashed = 1.hash
+  	each do |val|
+  		hashed += val.hash
+  		hashed = hashed.hash
+  	end
+  	hashed.hash
   end
 end
 
 class String
   def hash
+  	hashed = "a".ord.hash
+  	each_char do |char|
+  		hashed += char.ord.hash
+  		hashed = hashed.hash
+  	end
+  	hashed.hash
   end
 end
 
 class Hash
   def hash
+  	arr = []
+  	each do |key, val|
+  		arr << (key.hash + val.hash).hash
+  	end
+  	arr.sort.hash
   end
 end
